ff3d9b9479032c8b3e0c52936a26775688d44759
Scott Alguire <scott.alguire@gmail.com>
2017-07-20 19:35:14 -0400

    splitting reducers

--- js/main.js
+++ js/main.js
@@ -2,17 +2,26 @@ import 'babel-polyfill';
 import { createStore } from 'redux';
 import { VisibilityFilters, ADD_TODO, TOGGLE_TODO } from './actions';
 
+const { SHOW_ALL } = VisibilityFilters;
+
 const initialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
     todos: []
 }
 
-function todoApp(state = initialState, action) {
+// Smaller reducers
+
+function visibilityFilter(state = SHOW_ALL, action) {
     switch (action.type) {
         case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, {
-                visibilityFilter: action.filter
-            })
+            return action.filter
+        default:
+            return state
+    }
+}
+
+function todos(state = [], action) {
+    switch (action.type) {
         case ADD_TODO:
             return Object.assign({}, state, {
                 todos: [
@@ -37,4 +46,20 @@ function todoApp(state = initialState, action) {
         default:
             return state
     }
+}
+
+// Main reducer (Reducer Composition)
+function todoApp(state = initialState, action) {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({}, state, {
+                visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+            })
+        case ADD_TODO:
+        case TOGGLE_TODO:
+            return Object.assign({}, state, {
+                todos: todos(state.todos, action)
+            })
+    }
+    return state
 }
