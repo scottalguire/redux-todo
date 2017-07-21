5d660f32ffac2824cd7dc5f475bedb94aed8e99d
Scott Alguire <scott.alguire@gmail.com>
2017-07-21 02:12:48 -0400

    Testing some action dispatching. Imported actionTypes into reducers (critical for store to be updated on dispatches)

--- js/actions.js
+++ js/actions.js
@@ -1,30 +1,21 @@
 import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from './actionTypes';
 
 export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
 }
 
 // Action Creators
 
 export function addTodo(text) {
-    return {
-        type: ADD_TODO,
-        text
-    }
+    return { type: ADD_TODO, text }
 }
 
 export function toggleTodo(index) {
-    return {
-        type: TOGGLE_TODO,
-        index
-    }
+    return { type: TOGGLE_TODO, index }
 }
 
 export function setVisibilityFilter(filter) {
-    return {
-        type: SET_VISIBILITY_FILTER,
-        filter
-    }
+    return { type: SET_VISIBILITY_FILTER, filter }
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 import 'babel-polyfill';
 import { createStore } from 'redux';
-import { todoApp } from './reducers';
 
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './actions';
+import todoApp from './reducers';
+
+let store = createStore(
+    todoApp,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//console.log(store.getState());
+
+let unsubscribe = store.subscribe(() =>
+    console.log(store.getState())
+);
+
+//store.dispatch({type:'ADD_TODO', text:'Custom todo'})
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+unsubscribe();
