af54fb43c27b6495ef3ad36d1aa1f8c5f18f19f3
Scott Alguire <scott.alguire@gmail.com>
2017-07-21 15:43:04 -0400

    Testing a vanilla JS solution for binding state/dispatching actions with DOM elements. Switching to React next.

--- js/main.js
+++ js/main.js
@@ -23,4 +23,40 @@ store.dispatch(toggleTodo(0))
 store.dispatch(toggleTodo(1))
 store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
 
-unsubscribe();
+// Vanilla JS solution for binding DOM with state
+// Switching to React in next update
+
+document.getElementById('add-btn').addEventListener('click', function () {
+    console.log('click')
+    let txtBox = document.getElementById('todo-text').value;
+
+    store.dispatch(addTodo(txtBox))
+
+})
+
+document.getElementById('form').addEventListener('submit', function(event){
+    event.preventDefault();
+})
+
+function createList() {
+    let todoList = document.getElementById('todo-list');
+    todoList.innerHTML = "";
+        for (let i = 0; i < store.getState().todos.length; i++) {
+            let listItem = document.createElement("li");
+            let liText = document.createTextNode(store.getState().todos[i].text)
+
+            listItem.appendChild(liText);
+
+            let ul = document.getElementById('todo-list');
+            ul.appendChild(listItem)
+        }
+    }
+
+store.subscribe(() => {
+    createList()
+})
+//todoList.innerText = JSON.stringify(store.getState().todos.length)
+createList();
+
+
+unsubscribe()
