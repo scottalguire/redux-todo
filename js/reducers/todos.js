1860dd58a60a3ab71734ec2235a8db337dbae1e5
Scott Alguire <scott.alguire@gmail.com>
2017-08-11 17:21:41 -0400

    added id attribute to each todo item so that each todo can be toggled independently.

--- js/reducers/todos.js
+++ js/reducers/todos.js
@@ -7,13 +7,14 @@ function todos(state = [], action) {
             return [
                 ...state,
                 {
+                    id: action.id,
                     text: action.text,
                     completed: false
                 }
             ]
         case TOGGLE_TODO:
-            return state.map((todo, index) => {
-                if (index === action.index) {
+            return state.map((todo) => {
+                if (todo.id === action.id) {
                     return Object.assign({}, todo, {
                         completed: !todo.completed
                     })
